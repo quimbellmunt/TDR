a4dba5022cf3c55945258b8f53ad2fa08c762430
Andrea <andreabefu@cloud.com>
2019-07-18 18:18:29 +0200

    app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-var passport = require("passport");
+var passport = require("passport");//
 var bodyParser = require("body-parser");
 
 
@@ -12,12 +12,10 @@ var autoIncrement   = require('mongoose-auto-increment');
 var path = require('path');
 var express = require('express');
 var http = require('http');
-var mongoose = require('mongoose');
-var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1337);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('view options', { layout: false });
@@ -28,15 +26,15 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 
-mongoose.connect('mongodb://localhost:27017/TdR');
+mongoose.connect('mongodb://localhost:27017/Treball');
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const User = require('./models/user');
-const Tasques = require('./models/tasques');
+const User = require('./models/users');
 
 
 passport.use(new LocalStrategy(User.authenticate()));
@@ -48,71 +46,85 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.get('/', function (req, res) {
-  res.render('index', { user : req.user });
-});
-
-app.get('/register', function(req, res) {
-    res.render('index', { });
-});
-
-app.post('/register', function(req, res) {
-
-  User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
-      if (err) {
-          return res.render('index', { user : user });
-      }
-
-      passport.authenticate('local')(req, res, function () {
-        res.render('home');
-      });
+      res.render('index', { user : req.user });
   });
-});
 
-app.get('/login', function(req, res) {
-    res.render('index', { user : req.user });
-});
+  app.get('/register', function(req, res) {
+      res.render('index', { });
+  });
 
-app.post('/login', passport.authenticate('local'), function(req, res) {
-    console.log()
-    res.render('home');
-});
+  app.post('/register', function(req, res) {
 
-app.post('/tasques', function(req, res) {
-    Tasques.create(new Tasques({Nom : req.body.Nom,Descripcio: req.body.Descripcio,Preu: req.body.Preu,Temps: req.body.Temps}),function(err, Tasca) {
+    User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
         if (err) {
-          console.log(err)
+            return res.render('index', { user : user });
+        }
+
+        passport.authenticate('local')(req, res, function () {
           res.render('home');
-          }
-        res.render('home');
-      });    
-});
+        });
+    });
+  });
+
+  app.get('/login', function(req, res) {
+
+      res.render('index', { user : req.user });
+  });
+
+  app.post('/login', passport.authenticate('local'), function(req, res) {
+      res.render('home');
+  });
+
+  
+
+  app.post('/logout', function(req, res) {
+      req.logout();
+      res.render('index');
+  });
 
 
-
-
-
-app.post('/logout', function(req, res) {
-    req.logout();
-    res.render('index');
-});
-
-app.get('/tasques', function(req, res){
-    Tasques.find({},function(err, tasks){
-      console.log(tasks)
-      if(err) console.log(err)
-      res.render('tasques',{tasks:tasks}); 
-    })
-    
-});
+  app.get('/home', function(req, res){
+      res.render('home');
+  });
 
 
 app.listen(app.get('port'), function(){
   console.log(("Express server listening on port " + app.get('port')))
 });
 
+app.post('/create', function(req, res) {
+  tasques.register(new tasques({nom: req.body.nom, preu: req.body.preu, temps: req.body.temps, descripcio: req.body.descripcio}), function(err,tasques)
+  {if (err) 
+  console.log(err) 
 
+  res.render('tasques')
+  });
+});
 
+app.post('/modificaciou', function(req,res) {
+  usuari.findOneAndUpdate({nom:req.body.nom, cognoms:req.body.cognoms, username:req.body.username, mail:req.body.mail}, 
+    function(err,user) {
+      if(err) {
+        console.log(err)
+        
+      }else{
+        Trans.find({Receptor: user.id}, function(err, tasks){
+        res.render('home', {user, user, tasks:tasks});
+        }); 
+      }    
+    })
+});
 
+app.tasques('/cancelar', function(res) {
+  res.render('home')
+});
+  
 
+app.delete('/delete', function(req,res) {
+  req.delete();
+  res.render('home');
+});
 
-
+app.listen(app.get('port'), function(){
+  console.log(("Express server listening on port " + app.get('port')))
+});
