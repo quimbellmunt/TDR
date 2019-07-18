a488bb0d11300a6fff5dec5dbe8ba37409b71116
Andrea <andreabefu@cloud.com>
2019-07-18 19:59:41 +0200

    models i comentaris

--- models/transaccions.js
+++ models/transaccions.js
@@ -7,6 +7,28 @@ const Transaccio = new mongoose.Schema({
 	tasca: String,
 });
 
+var acceptada = true({  
+//Aquí teòricament si s'ha acceptat després de l'email, s'ha d'estar pendent de si la següent variable és true o false,
+// però ja veig que no es pot posar una varriable dintre d'una altre, el que passa és que no sé com fer-ho.   
+
+
+});
+
+
+var acabada = true;
+//(Poder posar a la web que l'has acabat o si l'has cancelat)
+
+var acabada = !false;
+// En el cas de cancelada s'envia mail (The Nodemailer module) a usuariOrigen per dir que s'ha cancelat. 
+
+var acceptada = false({
+ // Aquí s'hauria d'enviar un mail a la persona que ha enviat la tasca per comunicar que no ha estat acceptada. (inclou The Nodemailer module)
+
+
+});
+
+
+
 
 
 module.exports = mongoose.model('Transaccio', Transaccio);
