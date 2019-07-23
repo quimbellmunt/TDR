e0171115fadc4b73986cc3298cc8c4baa5a55c1b
Andrea <andreabefu@cloud.com>
2019-07-24 00:33:46 +0200

    ns que ha canviat...btw, he fet el dictionary

--- models/transaccions.js
+++ models/transaccions.js
@@ -6,7 +6,7 @@ const Transaccio = new mongoose.Schema({
 	usuariOrigen: String,
 	usuariReceptor: String, // si el tipus es creacio de tasca aqui li poses null
 	tasca: String,
-	preu: Number,
+	preu: Number, //la pregunta és com es sap quin preu és. No hauria de tenir una connexió amb la variable de tasques?
 	acceptada: Boolean,
 	acabada: Boolean,
 	identificadorTrans: Number
